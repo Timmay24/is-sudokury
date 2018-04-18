1c68f2e653c2faa23b831307be110998cc3ba4d1
abr002 <tim.hartig@haw-hamburg.de>
2018-04-18 12:03:22 +0200

    Add setter

--- src/haw/is/sudokury/Node.java
+++ src/haw/is/sudokury/Node.java
@@ -34,6 +34,10 @@ public class Node<T> {
         return children.isEmpty();
     }
 
+    public void setDifficulty(int difficulty) {
+        this.difficulty = difficulty;
+    }
+
     public int getDifficulty() {
         return difficulty;
     }
