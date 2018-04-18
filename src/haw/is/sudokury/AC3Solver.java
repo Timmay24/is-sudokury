42567e13303b8403e35cd48ad35320635097006f
AhmadHAW <Auren@gmx.net>
2018-04-18 14:55:42 +0200

    sad

---
+++ src/haw/is/sudokury/AC3Solver.java
@@ -0,0 +1,12 @@
+package haw.is.sudokury;
+
+public class AC3Solver extends Solver{
+
+	@Override
+	public int solve(int[][] board) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+}
---
+++ src/haw/is/sudokury/Field.java
@@ -0,0 +1,48 @@
+package haw.is.sudokury;
+
+public class Field {
+
+	private final int x;
+	private final int y;
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	
+	public Field(int xCord, int yCord) {
+		super();
+		this.x = xCord;
+		this.y = yCord;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + x;
+		result = prime * result + y;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Field other = (Field) obj;
+		if (x != other.x)
+			return false;
+		if (y != other.y)
+			return false;
+		return true;
+	}
+	
+}
