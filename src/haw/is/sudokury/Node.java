3c6d1ade331b44b69d5b0d7add004511fd268de7
abr002 <tim.hartig@haw-hamburg.de>
2018-04-18 12:00:38 +0200

    Add node class for tree building

---
+++ src/haw/is/sudokury/Node.java
@@ -0,0 +1,40 @@
+package haw.is.sudokury;
+
+import java.util.List;
+
+public class Node<T> {
+    private T data;
+    private List<Node<T>> children;
+
+    // Additional Attributes
+    private int difficulty;
+
+    public Node(T data) {
+        children = new ArrayList<>();
+        difficulty = 0;
+        this.data = data;
+    }
+
+    public Node<T> add(T data) {
+        Node<T> newChild = new Node<>(data);
+        children.add(newChild);
+        return newChild;
+    }
+
+    public boolean remove(Node<T> child) {
+        return children.remove(child);
+    }
+
+    // different name for getData for domain specific purpose
+    public T getBoard() {
+        return data;
+    }
+
+    public boolean isLeaf() {
+        return children.isEmpty();
+    }
+
+    public int getDifficulty() {
+        return difficulty;
+    }
+}
