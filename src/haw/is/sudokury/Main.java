d3d1db2e9378151754bc0e70bd8a6a5c391eba90
abr002 <tim.hartig@haw-hamburg.de>
2018-04-23 15:09:48 +0200

    Remove block comment

--- src/haw/is/sudokury/Main.java
+++ src/haw/is/sudokury/Main.java
@@ -3,11 +3,10 @@ package haw.is.sudokury;
 public class Main {
 
 	public static void main(String[] args) {
-		/*Solver solver = new AC3Solver();
+		Solver solver = new AC3Solver();
 		FieldCreator creator = new SudokuGenerator();
 		Controller controller = new Controller(solver, creator);
 		controller.printBoard(controller.getRoot().getBoard());
-		*/
 	}
 
 }
