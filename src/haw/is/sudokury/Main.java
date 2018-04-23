c2fdc8ae768f689a1191edf043b9b9ca2a7cf578
abr002 <tim.hartig@haw-hamburg.de>
2018-04-23 16:43:04 +0200

    Omit unused local reference

--- src/haw/is/sudokury/Main.java
+++ src/haw/is/sudokury/Main.java
@@ -1,11 +1,13 @@
 package haw.is.sudokury;
 
+import haw.is.sudokury.algorithms.AC3Solver;
+import haw.is.sudokury.algorithms.Solver;
+
 public class Main {
 
 	public static void main(String[] args) {
 		Solver solver = new AC3Solver();
-		FieldCreator creator = new SudokuGenerator();
-		Controller controller = new Controller(solver, creator);
+		Controller controller = new Controller(solver, new SudokuGenerator());
 		controller.printBoard(controller.getRoot().getBoard());
 	}
 
