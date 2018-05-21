107d8431398ef2e79fa7f63920726f7fc34c7296
Timmay <tim.hartig@haw-hamburg.de>
2018-05-21 19:06:17 +0200

    Remove unnecessary tests and fix namings

--- test/haw/is/sudokury/ConstraintBuilderTest.java
+++ test/haw/is/sudokury/ConstraintBuilderTest.java
@@ -3,7 +3,6 @@ package haw.is.sudokury;
 import haw.is.sudokury.constraints.ConstraintBuilder;
 import haw.is.sudokury.constraints.interfaces.Constraint;
 import haw.is.sudokury.models.Board;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Set;
@@ -13,12 +12,8 @@ import static org.junit.Assert.assertNotNull;
 
 public class ConstraintBuilderTest {
 
-	@Before
-	public void setUp() throws Exception {
-	}
-
 	@Test
-	public void test() {
+	public void testBuildSufficietConstraints() {
 		Set<Constraint> set = ConstraintBuilder.buildConstraints(new Board());
 		assertNotNull(set);
 		assertEquals(set.size(), 81*20);
--- test/haw/is/sudokury/FieldTest.java
+++ test/haw/is/sudokury/FieldTest.java
@@ -1,43 +1,18 @@
 package haw.is.sudokury;
 
 import haw.is.sudokury.models.Field;
-import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class FieldTest {
 
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	//teste das einfache Createn
 	@Test
 	public void testCreateField() {
 		Field field = Field.of(2, 1);
 		assertNotNull(field);
 		assertEquals(2,field.getX());
 		assertEquals(1,field.getY());
-	}
-	
-	//teste, ob wirklich die selbe Instanz zur�ck gegeben wird
-	@Test
-	public void testGetExistingField() {
-		Field field = Field.of(1, 1);
-		assertNotNull(field);
-		assertTrue(Field.of(1, 1) == field);
-	}
-
-	//teste, ob bei selber x, aber verschiedener y-koordinate der Prozess funktioniert
-	@Test
-	public void testCreateFieldYUnknown() {
-		Field field1 = Field.of(1, 1);
-		assertNotNull(field1);
-		Field field2 = Field.of(1, 2);
-		assertNotNull(field2);
-		assertNotEquals(field1, field2);
-		assertEquals(1, field1.getY());
-		assertEquals(2, field2.getY());
 	}
 }
